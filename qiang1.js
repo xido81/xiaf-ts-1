5e955269daf74d6aa208e045852c381242774a22
xido81 <68804262+xido81@users.noreply.github.com>
2021-01-10 22:34:55 +0800

    Create qiang1.js

--- qiang1.js
+++ qiang1.js
@@ -1,47 +1,3 @@
-xcaut2()
-
-function xcaut2() {
-    setTimeout(function(){
-        document.querySelector('.el-loading-text').innerHTML="load";
-        $("body").append(`
-        <div id="xcbx" class="xcbx">
-        
-        </div>`);
-        $("#xcbx").append(`
-        <div id="xcbxvar" class="xcbxvar">
-        
-        </div>`);
-        $("#xcbxvar").append(`<input type="text" id="xcbxvarv1" value="1"/>`);
-        $("#xcbx").append(`
-        <audio id="sound">
-            <source src="https://xido81.github.io/xiaf-ts-1/wapc/1/sound/error.ogg" type="audio/ogg">
-        </audio>
-        <audio id="dsound">
-            <source src="https://xido81.github.io/xiaf-ts-1/wapc/1/sound/uac.ogg" type="audio/ogg">
-        </audio>`);
-        
-        xcevent1();
-    },1000);
-    /*setTimeout(function(){
-        alert("qiang");
-    },5000);*/
-    
-}
-
-function xcevent1() {
-    var vr1=document.getElementById("xcbxvarv1").value;
-    var vrf1="1";
-    var vrf2="0";
-    if ( vr1==vrf1 ) {
-        $(".time").hide();
-    }
-    else if ( vr1==vrf2 ) {
-        $(".time").show();
-    }
-    setTimeout(function(){
-        xcevent1();
-    },700);
-}
 xcaut2();
 
 function xcaut2() {
@@ -75,7 +31,8 @@ function xcaut2g2() {
         <div id="xcbxvar" class="xcbxvar">
         
         </div>`);
-        $("#xcbxvar").append(`<input type="text" id="xcbxvarv1" value="1"/>`);
+        $("#xcbxvar").append(`<input type="text" id="xcbxvarv1" value="1"/>
+        <input type="text" id="xcbxvarv2" value="1"/>`);
         $("#xcbx").append(`
         <audio id="sound">
             <source src="https://xido81.github.io/xiaf-ts-1/wapc/1/sound/error.ogg" type="audio/ogg">
@@ -94,6 +51,7 @@ function xcaut2g2() {
 
 function xcevent1() {
     var vr1=document.getElementById("xcbxvarv1").value;
+    var vr2=document.getElementById("xcbxvarv2").value;
     var vrf1="1";
     var vrf2="0";
     if ( vr1==vrf1 ) {
@@ -102,6 +60,14 @@ function xcevent1() {
     else if ( vr1==vrf2 ) {
         $(".time").show();
     }
+    
+    if ( vr2==vrf1 ) {
+        $("[aria-describedby='el-popover-5297']").hide();
+    }
+    else if ( vr2==vrf2 ) {
+        $("[aria-describedby='el-popover-5297']").show();
+    }
+    
     setTimeout(function(){
         xcevent1();
     },700);

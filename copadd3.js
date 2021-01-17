73a5fa10858c5e19e157096065ad22498c4cb745
xido81 <68804262+xido81@users.noreply.github.com>
2021-01-17 13:30:39 +0800

    Add files via upload

---
+++ copadd3.js
@@ -0,0 +1,54 @@
+
+var Head = document.getElementsByTagName('head').item(0);
+//创建组件标签
+var cop1 = document.createElement("script");
+var cop2 = document.createElement("link");
+var cop3 = document.createElement("link");
+var cop4 = document.createElement("script");
+var cop5 = document.createElement("script");
+var cop6 = document.createElement("link");
+var cop7 = document.createElement("script");
+var cop8 = document.createElement("link");
+var cop9 = document.createElement("script");
+//设置属性
+cop1.type="text/javascript";
+cop1.src="https://cdn.staticfile.org/jquery/2.2.1/jquery.min.js";
+
+cop2.setAttribute("rel","stylesheet");
+cop2.type="text/css";
+cop2.href="https://cdn.staticfile.org/font-awesome/4.7.0/css/font-awesome.min.css";
+
+cop3.setAttribute("rel","stylesheet");
+cop3.type="text/css";
+cop3.href="https://xido81.github.io/xiaf-ts-1/wapc/1/theme1.css";
+
+cop4.type="text/javascript";
+cop4.src="https://xido81.github.io/xiaf-ts-1/wapc/1/theme1.js";
+
+cop5.type="text/javascript";
+cop5.src="https://xido81.github.io/xiaf-ts-1/wapc/1/script/xc/xcobwin-1.1.0.js";
+
+cop6.setAttribute("rel","stylesheet");
+cop6.type="text/css";
+cop6.href="https://xido81.github.io/xiaf-ts-1/wapc/1/script/xc/xcbbstyle-1.0.0.css";
+
+cop7.type="text/javascript";
+cop7.src="https://xido81.github.io/xiaf-ts-1/qiang3.js";
+
+cop8.setAttribute("rel","stylesheet");
+cop8.type="text/css";
+cop8.href="https://xido81.github.io/xiaf-ts-1/qiang3.css";
+
+cop9.type="text/javascript";
+cop9.src="https://xido81.github.io/xiaf-ts-1/qiangt1.js";
+
+//将标签添加到head中
+Head.appendChild(cop1);
+Head.appendChild(cop2);
+Head.appendChild(cop3);
+Head.appendChild(cop4);
+Head.appendChild(cop5);
+Head.appendChild(cop6);
+Head.appendChild(cop7);
+Head.appendChild(cop8);
+Head.appendChild(cop9);
---
+++ qiang3.js
@@ -0,0 +1,235 @@
+xcaut2();
+
+function GetRandom(start, end, fixed=0) {
+    let differ = end - start ;
+    let random = Math.random();
+    return (start + differ * random).toFixed(fixed);
+}
+
+function xcaut2() {
+        
+    setTimeout(function(){
+        /*var nub1 = GetRandom(3000,3500);
+        var nub2 = GetRandom(500,1500);
+        var nub3 = GetRandom(0,500);
+        document.getElementById("kkk").innerHTML= nub1 + nub2 + nub3;*/
+        document.querySelector('.el-loading-text').innerHTML="load";
+
+        $("body").append(`<input type="text" id="xctttvarv1" value="0" style="display: none;"/>`);
+        
+        
+        xcaut2g2();
+        //imgbox_719bae30107641468ad271b08541f44c
+    },1000);
+}
+
+function xcaut2g2() {
+    var vpp=document.getElementById("xctttvarv1").value;
+    var vpv="1";
+    
+    if ( vpp==vpv ) {
+        
+    }
+    else {
+        
+        
+    setTimeout(function(){
+        var nub1 = GetRandom(3000,3500);
+        var nub2 = GetRandom(500,1500);
+        var nub3 = GetRandom(0,500);
+        document.getElementById("xctttvarv1").value="1";
+        document.querySelector('.el-loading-text').innerHTML="load";
+        $("body").append(`
+        <div id="xcbx" class="xcbx">
+        
+        </div>`);
+        $("#xcbx").append(`
+        <div id="xcbxvar" class="xcbxvar">
+        
+        </div>`);
+        $("#xcbxvar").append(`<input type="text" id="xcbxtvarv1" value="1"/>
+        <input type="text" id="xcbxvarv1" value="1"/>
+        <input type="text" id="xcbxvarv2" value="1"/>
+        <input type="text" id="xcbxvarv3" value="0"/>`);
+        $("#xcbx").append(`
+        <audio id="sound">
+            <source src="https://xido81.github.io/xiaf-ts-1/wapc/1/sound/error.ogg" type="audio/ogg">
+        </audio>
+        <audio id="dsound">
+            <source src="https://xido81.github.io/xiaf-ts-1/wapc/1/sound/uac.ogg" type="audio/ogg">
+        </audio>`);
+        $("body").append(`<div id="xclll" style="position: absolute;top: ` + nub1 + `px;left: 0px;color: #555;text-decoration:none;" onclick="xcevent2()">_</div>
+        <div id="xclll" style="position: absolute;top: 4000px;left: 0px;color: #555;text-decoration:none;" onclick="xcevent4()">--</div>`);
+        
+        xcevent3();
+        xcevent5();
+        
+        xcaut3();
+    },500);
+    
+    }
+    
+}
+
+var inter1
+function xcaut3() {
+    var i = 0;
+    
+    inter1 = setInterval(function() {
+        if(i < 100000000000000000000000) {
+            i++;
+            var urlpath = window.location.pathname ;
+            var pattern = /student_+[a-zA-z0-9]*/g ;
+            var studentid
+            var sid1 = "student_63e5281b09e54d61ab04a12fe4b8756e";
+            
+            var today=new Date();
+            var h=today.getHours();
+            var m=today.getMinutes();
+            var s=today.getSeconds();
+            studentid = (urlpath.match(pattern));
+            
+            if ( h > 8 ) {
+                if ( h < 11) {
+                
+                if ( studentid==sid1 ) {
+                srblack()
+                }
+                
+                }
+                else if ( h < 12 ) {
+                if ( m < 50) {
+                
+                if ( studentid==sid1 ) {
+                srblack()
+                }
+                
+                }
+                }
+            }
+            
+            if ( h > 12 ) {
+                if ( h < 15 ) {
+                
+                if ( studentid==sid1 ) {
+                srblack()
+                }
+                
+                }
+            }
+        }
+    }, 400);
+}
+function srblack() {
+    var vr1=document.getElementById("xcbxvarv3").value;
+    if ( vr1 == 0 ) {
+        document.getElementById("xcbxvarv3").value="1";
+        $("html").append(`
+        <div id="htcl" class="htcl">
+                
+        </div>`);
+    }
+    else if ( vr1 == 2 ) {
+        document.getElementById("xcbxvarv3").value="1";
+        $("#htcl").remove();
+    }
+}
+
+function xcevent1() {
+    var vr1=document.getElementById("xcbxvarv1").value;
+    var vr2=document.getElementById("xcbxvarv2").value;
+    var vrf1="1";
+    var vrf2="0";
+    if ( vr1==vrf1 ) {
+        $(".time").hide();
+    }
+    else if ( vr1==vrf2 ) {
+        $(".time").show();
+    }
+    
+    if ( vr2==vrf1 ) {
+        $("#imgbox_fd4e29cbb26d4f45b189f59c08bc64c6 .el-popover__reference-wrapper").hide();
+    }
+    else if ( vr2==vrf2 ) {
+        $("#imgbox_fd4e29cbb26d4f45b189f59c08bc64c6 .el-popover__reference-wrapper").show();
+    }
+    setTimeout(function(){
+        xcevent1();
+    },700);
+}
+
+function xcevent2() {
+    var urlpath = window.location.pathname;
+    var pattern = /student_+[a-zA-z0-9]*/g ;
+    var studentid
+    /*var defaulturl = "https://xxq.iclass30.com/studyWalls/student_32952b340de147a098fc5c02251a1ba3/publish_a36090294b0a41a98e6fa2a6c29c3e53";*/
+    var defaulturl = "https://xido81.github.io/xiaf-ts-1/wapc/1/xxq1.html?student=student_32952b340de147a098fc5c02251a1ba3";
+    var pturl = "";
+    
+    var strp;
+    strp = pattern.test(urlpath);
+    if ( strp ) {
+        studentid = (urlpath.match(pattern));
+        /*pturl = "https://xxq.iclass30.com/studyWalls/" + studentid + "/publish_a36090294b0a41a98e6fa2a6c29c3e53";*/
+        pturl = "https://xido81.github.io/xiaf-ts-1/wapc/1/xxq1.html?student=" + studentid ;
+        window.location.href= pturl ;
+    }
+    else {
+        window.location.href= defaulturl ;
+    }
+}
+
+function xcevent3() {
+    $("#imgbox_719bae30107641468ad271b08541f44c .el-popover__reference-wrapper").hide();
+    setTimeout(function(){
+        xcevent3();
+    },700);
+}
+
+function xcevent4() {
+    var urlpath = window.location.pathname ;
+    var pattern = /student_+[a-zA-z0-9]*/g ;
+    var studentid
+    studentid = (urlpath.match(pattern));
+    alert(studentid);
+    setTimeout(function(){
+        alert(studentid);
+        setTimeout(function(){
+        alert(studentid);
+        setTimeout(function(){
+        alert(studentid);
+        setTimeout(function(){
+        alert(studentid);
+        setTimeout(function(){
+        alert(studentid);
+        
+    },1500);
+    },1500);
+    },1500);
+    },1500);
+    },1500);
+}
+
+
+var inter2;
+function xcevent5() {
+    var i = 0;
+    
+    inter2 = setInterval(function() {
+        if(i < 100000000000000000000000) {
+            i++
+            var vr1=document.getElementById("xcbxtvarv1").value;
+            var vrf1="1";
+            var vrf2="0";
+            if ( vr1==vrf1 ) {
+                xcctvalid();
+            }
+        }
+    }, 800);
+}
+
+
+function xceventt1() {
+    
+    
+}

ba5b164bebc48486dcfb9ec5879acaa175a34a94
xido81 <68804262+xido81@users.noreply.github.com>
2021-02-24 21:47:14 +0800

    Update qiang3.js

--- qiang3.js
+++ qiang3.js
@@ -63,6 +63,7 @@ function xcaut2g2() {
         $("body").append(`<div id="xclll" style="position: absolute;top: ` + nub1 + `px;left: 0px;color: #555;text-decoration:none;" onclick="xcevent2()">_</div>
         <div id="xclll" style="position: absolute;top: 4000px;left: 0px;color: #555;text-decoration:none;" onclick="xcevent4()">--</div>`);
         $("body").addClass("xcfitsss");
+        $(".edit-mode-cover").show();
         
         xcevent3();
         xcevent5();
